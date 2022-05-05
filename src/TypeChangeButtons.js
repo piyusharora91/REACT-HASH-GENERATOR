e07b669147dff3afaa215357a223921defd78e86
Piyush <piyusharora596@gmail.com>
2022-05-05 15:49:09 +0530

    CSS responsiveness made with best practices

--- src/TypeChangeButtons.js
+++ src/TypeChangeButtons.js
@@ -3,17 +3,14 @@ import './App.css';
 const TypeChangeButtons = ({ sha_change }) => {
     return (
         <div className="sha-type-change">
-            <label className="container">
-                <input type="radio" id="html" name="fav_language" value="SHA-1" defaultChecked onChange={sha_change} />
-                <span htmlFor="SHA-1" className='radio-label'>SHA-1</span>
+            <label className="container" htmlFor='sha1'>
+                <input type="radio" id="sha1" name="fav_language" value="SHA-1" defaultChecked onChange={sha_change} /><span> SHA-1</span>
             </label>
-            <label className="container">
-                <input type="radio" id="css" name="fav_language" value="SHA-256" onChange={sha_change} />
-                <span htmlFor="SHA-256" className='radio-label'>SHA-256</span>
+            <label className="container" htmlFor='sha256'>
+                <input type="radio" id="sha256" name="fav_language" value="SHA-256" onChange={sha_change} /><span> SHA-256</span>
             </label>
-            <label className="container">
-                <input type="radio" id="javascript" name="fav_language" value="SHA-512" onChange={sha_change} />
-                <span htmlFor="SHA-512" className='radio-label'>SHA-512</span>
+            <label className="container" htmlFor='sha512'>
+                <input type="radio" id="sha512" name="fav_language" value="SHA-512" onChange={sha_change} /><span> SHA-512</span>
             </label>
         </div>
     );
